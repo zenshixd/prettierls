840846747d6cb5a1ba7b3cdf53b9c38c5c7cdf50
zenshixd <zenshixd@proton.me>
2025-01-19 17:42:53 +0100

    change formatting to be done eagerly on every change instead of upon request

--- index.ts
+++ index.ts
@@ -8,15 +8,79 @@ import {
   TextDocumentSyncKind,
 } from "vscode-languageserver/node";
 import type * as PrettierModule from "prettier";
-import { TextDocument } from "vscode-languageserver-textdocument";
+import {
+  TextDocument,
+  type TextDocumentContentChangeEvent,
+  type TextEdit,
+} from "vscode-languageserver-textdocument";
 
 const prettierCache: Map<string, typeof PrettierModule> = new Map();
-const prettierConfigCache: Map<string, PrettierModule.Config> = new Map();
+
+interface FormattedTextDocument extends TextDocument {
+  config: Promise<PrettierModule.Config | null>;
+  formatted: Promise<string>;
+}
+
+const FormattedTextDocument = {
+  create(uri: string, languageId: string, version: number, text: string) {
+    const document = TextDocument.create(
+      uri,
+      languageId,
+      version,
+      text,
+    ) as FormattedTextDocument;
+
+    const prettier: typeof PrettierModule = require("prettier");
+    prettierCache.set(uri, prettier);
+
+    const prettierConfigPromise = prettier.resolveConfig(uri, {
+      useCache: false,
+      editorconfig: true,
+    });
+
+    document.config = prettierConfigPromise;
+    document.formatted = prettierConfigPromise.then((config) =>
+      prettier.format(text, {
+        ...(config ?? {}),
+        filepath: uri,
+      }),
+    );
+    return document;
+  },
+  update(
+    document: FormattedTextDocument,
+    changes: TextDocumentContentChangeEvent[],
+    version: number,
+  ) {
+    const newDocument = TextDocument.update(
+      document,
+      changes,
+      version,
+    ) as FormattedTextDocument;
+
+    const prettier = prettierCache.get(newDocument.uri);
+    if (prettier) {
+      newDocument.formatted = newDocument.config.then((config) =>
+        prettier.format(newDocument.getText(), {
+          ...(config ?? {}),
+          filepath: newDocument.uri,
+        }),
+      );
+    }
+
+    return newDocument;
+  },
+  applyEdits(document: FormattedTextDocument, edits: TextEdit[]) {
+    return TextDocument.applyEdits(document, edits);
+  },
+};
 
 const require = createRequire(import.meta.url);
 export function run() {
   const connection = createConnection(ProposedFeatures.all);
-  const documents = new TextDocuments(TextDocument);
+  const documents = new TextDocuments<FormattedTextDocument>(
+    FormattedTextDocument,
+  );
 
   connection.onInitialize(() => {
     return {
@@ -28,44 +92,25 @@ export function run() {
   });
 
   documents.onDidOpen(async (params) => {
-    if (!prettierCache.has(params.document.uri)) {
-      const prettierPath = require.resolve("prettier", {
-        paths: [params.document.uri.replace(/^file:\/\//g, "")],
-      });
-      const prettier: typeof PrettierModule = require(prettierPath);
-      prettierCache.set(params.document.uri, prettier);
-
-      const prettierConfig = await prettier.resolveConfig(params.document.uri, {
-        useCache: false,
-        editorconfig: true,
-      });
-      prettierConfigCache.set(params.document.uri, prettierConfig ?? {});
+    const document = documents.get(params.document.uri);
+    if (!document) {
+      return;
     }
 
+    await document.config;
     await connection.sendNotification("prettier/loaded");
   });
 
   connection.onDocumentFormatting(async (params) => {
-    const prettier = prettierCache.get(params.textDocument.uri);
-    if (!prettier) {
-      return [];
-    }
-
-    const prettierConfig = prettierConfigCache.get(params.textDocument.uri)!;
     const document = documents.get(params.textDocument.uri);
 
     if (!document) {
       return [];
     }
 
-    const newText = await prettier.format(document.getText(), {
-      ...prettierConfig,
-      filepath: params.textDocument.uri,
-    });
-
     return [
       {
-        newText,
+        newText: await document.formatted,
         range: Range.create(
           Position.create(0, 0),
           Position.create(document.lineCount, 0),
@@ -76,7 +121,6 @@ export function run() {
 
   documents.onDidClose((params) => {
     prettierCache.delete(params.document.uri);
-    prettierConfigCache.delete(params.document.uri);
   });
 
   documents.listen(connection);
